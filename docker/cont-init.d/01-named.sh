c264ad2f2abd4d4480b6ac64599c05364764674d
RootShell-coder <Root.Shelling@gmail.com>
2026-04-20 05:03:58 +0300

    DNS authoritative

--- docker/cont-init.d/01-named.sh
+++ docker/cont-init.d/01-named.sh
@@ -1,40 +1,15 @@
 #!/bin/sh
 
 log() {
-    echo -e "\033[0;32m[$(date '+%Y-%m-%d %H:%M:%S')] $1\033[0m"
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1"
 }
 
-log "Generate TSIG key for DDNS"
-tsig-keygen ddns-key > /etc/bind/ddns.key
-chown named:named /etc/bind/ddns.key
-cat  /etc/bind/ddns.key
-
-log "Generate rndc key for remote control"
+log "Generate rndc key"
 rndc-confgen -a -c /etc/bind/rndc.key
-chown named:named /etc/bind/rndc.key
-cat /etc/bind/rndc.key
 
-log "Download root hints"
-curl -SsL --max-time 30 https://www.internic.net/domain/named.root -o /etc/bind/named.root
-chown named:named /etc/bind/named.root
+log "Create directories"
+mkdir -p /var/cache/bind/keys
+chown -R named:named /var/cache/bind /etc/bind
 
-log "Create cache directories"
-mkdir -p /var/cache/bind/slaves
-mkdir -p /var/cache/bind/managed-keys
-chown -R named:named /var/cache/bind
-chown named:named /etc/bind/
-
-log "Set permissions"
-chmod 700 /var/cache/bind
-chmod 700 /etc/bind/
-chmod 600 /etc/bind/ddns.key
-chmod 640 /etc/bind/rndc.key
-
-log "TLS Dot"
-mkdir -p /etc/ssl/bind/
-chown -R named:named /etc/ssl/bind
-chmod 644 "/etc/ssl/bind/server.crt"
-chmod 640 "/etc/ssl/bind/server.key"
-
-log "Check configuration before starting"
+log "Check config"
 named-checkconf /etc/bind/named.conf
--- example/bind/zones/dnssec/db.example.com
+++ example/bind/zones/dnssec/db.example.com
@@ -1,12 +1,12 @@
 $TTL 86400
-@ IN SOA ns.example.com. noreply.example.com. (
-    2023121901 ; serial
+@ IN SOA ns.example.com. admin.example.com. (
+    2024042001 ; serial
     3600       ; refresh
     1800       ; retry
     604800     ; expire
     86400      ; minimum
 )
-
 @ IN NS ns.example.com.
 ns IN A 127.0.0.1
 @ IN A 127.0.0.1
+www IN A 127.0.0.1
